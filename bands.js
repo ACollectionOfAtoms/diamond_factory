5d6440ccc4f6b57245e4acb5c10f5b6e7e48b0e0
acollectionofatoms <hernandezdavid88@utexas.edu>
2015-10-02 19:10:58 -0500

    Modified carousel

--- bands.js
+++ bands.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     $("#band-covers").owlCarousel({
+        items: 1,
         singleItem: true,
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        lazyLoad: true,
     });
 });
